dd1ac20295212e922e928cb9643ac59b81984a24
Kehara04 <keharawijesinghe04@gmail.com>
2025-08-29 12:11:39 +0530

    recycle login complete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,52 @@
+// // userRoutes.js
+// const express = require('express');
+// const router = express.Router();
+
+// const userController = require('../controllers/userController'); 
+// const upload = require("../middleware/updateMiddleware");
+// const authMiddleware = require('../middleware/authMiddleware');
+
+// router.get('/', userController.getUsers);
+
+// router.get("/user", userController.getUserById);
+
+// router.post('/signin', userController.login); 
+
+// router.get('/me', authMiddleware(['admin', 'customer', 'inventory_manager', 'customer_supporter', 'deliver']), userController.authentication);
+
+// router.get("/verify/:token", userController.verifyemail);
+
+// router.put("/updateuser",upload.single('profile_image'), userController.updateUser);
+
+// router.post('/forgotpassword', userController.forgotPassword);
+
+// router.post('/reset-password/:token', userController.resetPassword);
+
+// router.delete("/deleteuser", userController.deleteUser)
+
+
+// router.get('/admin-dashboard', authMiddleware(['admin']), (req, res) => {
+//     res.json({ message: 'Welcome to Admin Dashboard' });
+//   });
+  
+//   router.get('/customer-dashboard', authMiddleware(['customer']), (req, res) => {
+//     res.json({ message: 'Welcome to Customer Dashboard' });
+//   });
+  
+//   router.get('/inventory-dashboard', authMiddleware(['inventory_manager']), (req, res) => {
+//     res.json({ message: 'Welcome to Inventory Dashboard' });
+//   });
+  
+//   router.get('/support-dashboard', authMiddleware(['customer_supporter']), (req, res) => {
+//     res.json({ message: 'Welcome to Customer Support Dashboard' });
+//   });
+  
+//   router.get('/deliver-dashboard', authMiddleware(['deliver']), (req, res) => {
+//     res.json({ message: 'Welcome to Deliver Dashboard' });
+//   });
+
+// module.exports = router;
+
 // userRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -12,7 +61,7 @@ router.get("/user", userController.getUserById);
 
 router.post('/signin', userController.login); 
 
-router.get('/me', authMiddleware(['admin', 'customer', 'inventory_manager', 'customer_supporter', 'deliver']), userController.authentication);
+router.get('/me', authMiddleware(['admin', 'customer']), userController.authentication);
 
 router.get("/verify/:token", userController.verifyemail);
 
@@ -33,17 +82,19 @@ router.get('/admin-dashboard', authMiddleware(['admin']), (req, res) => {
     res.json({ message: 'Welcome to Customer Dashboard' });
   });
   
-  router.get('/inventory-dashboard', authMiddleware(['inventory_manager']), (req, res) => {
-    res.json({ message: 'Welcome to Inventory Dashboard' });
-  });
+  // router.get('/recycle_centre-dashboard', authMiddleware(['recycleCentre']), (req, res) => {
+  //   res.json({ message: 'Welcome to Recycle Centre Dashboard' });
+  // });
   
-  router.get('/support-dashboard', authMiddleware(['customer_supporter']), (req, res) => {
-    res.json({ message: 'Welcome to Customer Support Dashboard' });
-  });
+//   router.get('/support-dashboard', authMiddleware(['customer_supporter']), (req, res) => {
+//     res.json({ message: 'Welcome to Customer Support Dashboard' });
+//   });
   
-  router.get('/deliver-dashboard', authMiddleware(['deliver']), (req, res) => {
-    res.json({ message: 'Welcome to Deliver Dashboard' });
-  });
+//   router.get('/deliver-dashboard', authMiddleware(['deliver']), (req, res) => {
+//     res.json({ message: 'Welcome to Deliver Dashboard' });
+//   });
 
 module.exports = router;
+
+
 
